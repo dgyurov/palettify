1ca987d0d3a593ebc86e1e65e175981ed69a84fe
Dimitar Gyurov <dgyurov@Dimitars-MacBook-Pro.local>
2015-12-04 03:58:43 +0100

    Added animations and additional functionality

--- app/app.js
+++ app/app.js
@@ -1,7 +1,10 @@
 'use strict';
-angular.module('PalettifyApp', [])
- .controller('MainController', function($scope,$http) {
+angular.module('PalettifyApp', ['ngAnimate'])
+ .controller('MainController', function($scope,$http,$timeout) {
+
      $scope.pContainer = true;
+     $scope.backgroundclr = 'whitebg';
+
      $http({method: 'GET', url: 'http://www.colourlovers.com/api/palettes/top?numResults=100&format=json'})
      .success(function(data) {
        $scope.palette = Math.floor(Math.random()*data.length+1);
@@ -16,12 +19,25 @@ angular.module('PalettifyApp', [])
        console.log('Ooooops! Something went wrong Charlie!');
      });
 
-     $scope.nextPalette = function(){
-       var last = $scope.palette;
-       var next  = -1;
-       while(next===-1||next===last){
-         next = Math.floor(Math.random()*$scope.colors.length+1);
+     $scope.nextPalette = function($event){
+       $event.stopPropagation();
+       $scope.pContainer = false;
+       $timeout(function(){
+         var last = $scope.palette;
+         var next  = -1;
+         while(next===-1||next===last){
+           next = Math.floor(Math.random()*$scope.colors.length+1);
+         }
+         $scope.palette = next;
+         $scope.pContainer = true;
+       }, 500);
+     };
+
+     $scope.changeBackground = function(){
+       if ($scope.backgroundclr==='whitebg') {
+         $scope.backgroundclr = 'darkbg';
+       }else{
+         $scope.backgroundclr = 'whitebg';
        }
-       $scope.palette = next;
      };
 });

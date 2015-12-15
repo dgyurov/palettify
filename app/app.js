7e25e298ab4b3b889ba63aa80abf74cdefcd1c65
Dimitar Gyurov <dgyurov@Dimitars-MacBook-Pro.local>
2015-12-15 15:51:35 +0100

    added color inversion for the hex

--- app/app.js
+++ app/app.js
@@ -1,3 +1,7 @@
+/*jshint bitwise: false*/
+/*global angular:false */
+/*global document:false */
+
 'use strict';
 angular.module('PalettifyApp', ['ngAnimate'])
  .controller('MainController', function($scope,$http,$timeout) {
@@ -12,10 +16,13 @@ angular.module('PalettifyApp', ['ngAnimate'])
        $scope.palette = Math.floor(Math.random()*data.length);
        $scope.titles = [];
        $scope.colors = [];
+       $scope.lumi = [];
+
        for (var i in data) {
          $scope.colors.push(data[i].colors);
          $scope.titles.push(data[i].title);
        }
+
        angular.element(document.getElementsByClassName('tooltipnow')).tooltip();
        $scope.dataLoaded = true;
      })
@@ -46,6 +53,21 @@ angular.module('PalettifyApp', ['ngAnimate'])
          $scope.backgroundclr = 'whitebg';
          $scope.buttonclr = 'darkbg';
          $scope.invert=true;
+       }
+     };
+
+     $scope.isLight = function(hex){
+       var rgb,r,g,b,l;
+       rgb = parseInt(hex, 16);
+       r = (rgb >> 16) & 0xff;
+       g = (rgb >>  8) & 0xff;
+       b = (rgb >>  0) & 0xff;
+       l = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+       console.log('Lumi is: '+l);
+       if(l>220){
+         return true;
+       }else{
+         return false;
        }
      };
 });
